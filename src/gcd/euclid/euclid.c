8f9fe05d4ab98ddfadb9b617271e78376df4170e
momeemt <momiimt@gmail.com>
2023-10-09 14:56:31 +0900

    implemented gcd euclid

---
+++ src/gcd/euclid/euclid.c
@@ -0,0 +1,20 @@
+#include "gcd/euclid/euclid.h"
+
+int gcd_euclid(int n, int m) {
+    while (n != m) {
+        if (n > m) {
+            n -= m;
+        } else {
+            m -= n;
+        }
+    }
+    return n;
+}
+
+int gcd_recursive(int n, int m) {
+    int r = n % m;
+    if (r == 0) {
+        return m;
+    }
+    return gcd_recursive(m, r);   
+}
