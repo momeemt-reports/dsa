ba557704f34caec6722f038b3767e68a76ffad41
momeemt <momiimt@gmail.com>
2023-10-19 15:08:03 +0900

    add: recursive fibonacci

---
+++ src/fibonacci/recursive/recursive.c
@@ -0,0 +1,14 @@
+#include "fibonacci.h"
+
+int called_fibonacci = 0;
+
+int fibonacci_recursive (int n) {
+    called_fibonacci++;
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1; 
+    } else {
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
+    }
+}
