ca58efe9298d9a8246cbf5e158bdd021475f8a3a
momeemt <momiimt@gmail.com>
2023-10-28 16:11:59 +0900

    add: linked_list template

---
+++ headers/linked_list.h
@@ -0,0 +1,17 @@
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
+
+typedef struct cell {
+    int data;
+    struct cell *next;
+} Cell;
+
+extern Cell *head;
+
+void insert_cell(Cell *p, int d);
+void insert_cell_top(int d);
+void delete_cell(Cell *p);
+void delete_cell_top(void);
+void display(void);
+
+#endif // __LINKED_LIST_H__
---
+++ src/linked_list/linked_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+Cell *head = NULL;
+
+void insert_cell(Cell *p, int d) {
+
+}
+
+void insert_cell_top(int d) {
+    Cell *new_cell = (Cell *)malloc(sizeof(Cell));
+    new_cell->data = d;
+    new_cell->next = head;
+    head = new_cell;
+}
+
+void delete_cell(Cell *p) {
+
+}
+
+void delete_cell_top(void) {
+
+}
+
+void display(void) {
+
+        }
