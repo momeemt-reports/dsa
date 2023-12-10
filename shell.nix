1e8547a053b549ad33487048253e293e94e3ab33
Mutsuha Asada <me@momee.mt>
2023-12-10 21:49:15 +0900

    implemented sort algorithm

--- shell.nix
+++ shell.nix
@@ -8,4 +8,10 @@ pkgs.mkShell {
     cunit
     doxygen
   ];
+
+  shellHook = ''
+    export C_INCLUDE_PATH=$(clang -E -x c - -v < /dev/null 2>&1 | sed -n '/#include <...> search starts here:/,/End of search list./p' | grep -vE '#include|End of search list.' | sed 's/ (framework directory)//' | sed 's/^ //' | tr '\n' ':' | sed 's/:$$//')
+    export C_INCLUDE_PATH=$C_INCLUDE_PATH$(pwd)"/headers"
+    export CUNIT_INCLUDE_DIR="${pkgs.cunit}/include"
+  '';
 }
