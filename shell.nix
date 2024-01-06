039ec6543b53b96451aa4dcee4e8c1a1d34d768f
Mutsuha Asada <me@momee.mt>
2024-01-06 23:56:38 +0900

    updated nixpkgs and added cmake to buildInputs

--- shell.nix
+++ shell.nix
@@ -1,11 +1,24 @@
 { pkgs ? import ./nix/pkgs.nix }:
+let
+  libcIncludeDir = if pkgs.stdenv.isDarwin then
+    "${pkgs.darwin.Libsystem}/include"
+  else
+    "${pkgs.glibc.dev}/include";
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     llvmPackages_16.libcxxClang
     llvmPackages_16.clang-unwrapped
     gnumake
+    cmake
     gnuplot
     cunit
     doxygen
-  ];
+    nil
+  ] ++ pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.darwin.Libsystem
+    ++ pkgs.lib.optional (!pkgs.stdenv.isDarwin) pkgs.glibc.dev;
+
+  CUNIT_INCLUDE_DIR = "${pkgs.cunit}/include";
+  LIBC_INCLUDE_DIR = libcIncludeDir;
+  CLANG_INCLUDE_DIR = "${pkgs.llvmPackages_16.libcxxClang}/resource-root/include";
 }
