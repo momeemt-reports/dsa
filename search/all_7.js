f88bd3829ee9f447e8a3c9ec51e69dc1698476f0
momeemt <momeemt@users.noreply.github.com>
2024-01-06 15:00:27 +0000

    deploy: daa67f34bfdfd6e92455c93e72a1611b33b569e8

--- search/all_7.js
+++ search/all_7.js
@@ -5,5 +5,5 @@ var searchData=
   ['hash_5ftable_5fprime_2',['HASH_TABLE_PRIME',['../open__addressing_8h.html#adfeca8fe76c610fd3ca8ea91d6703a49',1,'open_addressing.h']]],
   ['head_3',['head',['../linked__list_8h.html#a44a1f05f01693276467f8486df44befa',1,'linked_list.h']]],
   ['heap_5fsort_4',['heap_sort',['../sort__collection_8h.html#ae5f12167901772cd7c99c63b9b4cac80',1,'sort_collection.h']]],
-  ['height_5',['height',['../structnode.html#a522298f219c14128a6027ee43046ea15',1,'node::height()'],['../binarytree_8h.html#a4af6752a1b7e972b6b108010956f41b2',1,'height():&#160;binarytree.h']]]
+  ['height_5',['height',['../structnode.html#a522298f219c14128a6027ee43046ea15',1,'node::height'],['../binarytree_8h.html#a4af6752a1b7e972b6b108010956f41b2',1,'height():&#160;binarytree.h']]]
 ];
--- search/typedefs_3.js
+++ search/typedefs_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['node_0',['Node',['../binarysearchtree_8h.html#ac09cf950484bd9550d14d602b0e5e7fb',1,'Node():&#160;binarysearchtree.h'],['../binarytree_8h.html#ac09cf950484bd9550d14d602b0e5e7fb',1,'Node():&#160;binarytree.h'],['../bst__advanced_8h.html#ac09cf950484bd9550d14d602b0e5e7fb',1,'Node():&#160;bst_advanced.h']]]
+  ['node_0',['Node',['../binarysearchtree_8h.html#ac09cf950484bd9550d14d602b0e5e7fb',1,'Node:&#160;binarysearchtree.h'],['../binarytree_8h.html#ac09cf950484bd9550d14d602b0e5e7fb',1,'Node:&#160;binarytree.h'],['../bst__advanced_8h.html#ac09cf950484bd9550d14d602b0e5e7fb',1,'Node:&#160;bst_advanced.h']]]
 ];
